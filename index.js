46c94624a0539f8e107c2d4b717588ce045b0582
Nur Febriana Bakri <nurfebrianab011@gmail.com>
2025-11-05 22:29:06 +0800

    Menambahkan fitur CRUD dasar (getTasks dan addTask) dengan async/await

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
 const fs = require('fs/promises');
 
+// Membaca daftar tugas dari file tasks.json
+async function getTasks() {
+  try {
+    const data = await fs.readFile('tasks.json', 'utf8');
+    const tasks = JSON.parse(data);
+    return tasks;
+  } catch (err) {
+    console.error('Gagal membaca file tasks.json:', err);
+    return []; 
+  }
+}
+
+// Menambah tugas baru
+async function addTask(title) {
+  try {
+   
+    const tasks = await getTasks();
+
+    const newTask = {
+      id: tasks.length + 1,
+      title: title,
+      done: false
+    };
+
+    tasks.push(newTask);
+
+    await fs.writeFile('tasks.json', JSON.stringify(tasks, null, 2));
+
+    console.log(`Tugas "${title}" berhasil ditambahkan!`);
+  } catch (err) {
+    console.error('Gagal menulis ke file tasks.json:', err);
+  }
+}
+
 async function main() {
-  const data = await fs.readFile('tasks.json', 'utf8');
-  console.log('Isi file:', data);
+  await addTask('Belajar Node.js');
+  const allTasks = await getTasks();
+  
+  console.log('Daftar Tugas:');
+  allTasks.forEach(task => {
+    console.log(`${task.id}. ${task.title} - ${task.done ? 'Selesai' : 'Belum'}`);
+  });
 }
 
 main();
